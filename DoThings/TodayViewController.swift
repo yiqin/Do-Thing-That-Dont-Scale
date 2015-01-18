92f9dc15d84c547b9e9566c4a868e91af1d9ce2e
yiqin <yiqin.mems@gmail.com>
2015-01-18 09:29:07 -0500

    Parse.com works now.

--- DoThings/TodayViewController.swift
+++ DoThings/TodayViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 import NotificationCenter
+import Parse
 
 class TodayViewController: UIViewController, NCWidgetProviding {
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
+        println("hello")
+        loadDataFromParse()
+    }
+    
+    func loadDataFromParse(){
+        Parse.setApplicationId("2D6T3tgwBIPoE8HkuninwT3gsUkrHouCfzg0MzDL", clientKey: "cmvDVWTEIrZO4phyuddppS96diUqckCKazxBEwxH")
+        
+        let query = PFQuery(className: "TodayProduct")
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock { (objects:[AnyObject]!, error:NSError!) -> Void in
+            
+            println("when you see this .... it means you have data from Parse.com. ")
+            
+            
+        }
+        
     }
     
     override func didReceiveMemoryWarning() {
